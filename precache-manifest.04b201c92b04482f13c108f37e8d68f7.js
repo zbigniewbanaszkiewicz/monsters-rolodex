5ceeb29463cf7909e9dcaed9210c3e0d5076668f
zbigniewbanaszkiewicz <zbigniewbanaszkiewicz70@gmail.com>
2020-02-04 17:36:51 +0100

    Updates

---
+++ precache-manifest.04b201c92b04482f13c108f37e8d68f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9290d09e735a9a28b5d6de4030fbec8a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "915b5ed306d3c7e64fe1",
+    "url": "/monsters-rolodex/static/css/main.09c41e51.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915b5ed306d3c7e64fe1",
+    "url": "/monsters-rolodex/static/js/main.2fffde6f.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
